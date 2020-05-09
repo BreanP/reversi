746b84146bfc4dd0f2d3080f1911510fab24d286
BreanP <bpavlina@uci.edu>
2020-05-09 18:38:35 -0400

    no message

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 /* functions for general use */
-function GetURLParameters(whichParam)
+function getURLParameters(whichParam)
   var pageURL = window.location.search.substring(1);
   var pageURLVariables = pageURL.split('&');
   for(var i = 0; i < pageURLVariables.length; i++){
@@ -8,3 +8,10 @@ function GetURLParameters(whichParam)
       return parametername[1];
     }
   }
+
+var username = getURLParameters('username');
+if('undefined' == typeof username || !username){
+  username = 'Anonymous_'+Math.random();
+}
+
+$('#messages').append('<h4>'+username+'</h4>');
