3fa2423b903cfa2196b0777ef82f0ecdc1e357eb
BreanP <bpavlina@uci.edu>
2020-05-10 11:32:04 -0400

    no message

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ function getURLParameters(whichParam)
   for(var i = 0; i < pageURLVariables.length; i++){
     var parameterName = pageURLVariables[i].split('=');
     if(parameterName[0] == whichParam){
-      return parametername[1];
+      return parameterName[1];
     }
 }
 
